50a0087566c53b2d6db3e98554a4572609a066fb
Will Barker <willbarker34@gmail.com>
2024-01-02 12:33:12 -0500

    revised form, removed input prompt updates on front end in favor of sending directly to prompt endpoint

--- fixit-webapp/app/components/prompt/prompt.js
+++ fixit-webapp/app/components/prompt/prompt.js
@@ -1,26 +1,29 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export function NewPrompt({ initialPrompt = "" }) {
-  const [inputValue, setInputValue] = useState(initialPrompt);
-  const [enteredPrompt, setEnteredPrompt] = useState(initialPrompt);
+  const promptInputRef = useRef();
 
-  const changeInputHandler = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const submitHandler = (event) => {
+  const submitFormHandler = (event) => {
     event.preventDefault();
-    setEnteredPrompt(inputValue);
-    console.log("Submitted prompt:", inputValue);
+
+    const enteredPrompt = promptInputRef.current.value;
+    const requestBody = { prompt: enteredPrompt }
+    fetch("/api/prompts", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Fixit</h1>
       <p className="text-lg font-semibold mb-4">Enter your problem below:</p>
-      <form onSubmit={submitHandler} className="flex flex-col items-center">
+      <form onSubmit={submitFormHandler} className="flex flex-col items-center">
         <label htmlFor="prompt" className="sr-only">
           Enter your problem
         </label>
@@ -28,10 +31,9 @@ export function NewPrompt({ initialPrompt = "" }) {
           <input
             type="text"
             id="prompt"
-            value={inputValue}
-            onChange={changeInputHandler}
             required
             className="outline outline-2 outline-offset-2 outline-stone-300 h-16 w-96 pr-8 pl-5 focus:shadow rounded-2xl"
+            ref={promptInputRef}
           />
           <button
             type="submit"
@@ -41,7 +43,6 @@ export function NewPrompt({ initialPrompt = "" }) {
           </button>
         </div>
       </form>
-      <p className="text-lg font-semibold mt-4">{enteredPrompt}</p>
     </div>
   );
 }
