bf3e02a5c791b17dff8392ace0f4a176beda440b
Will Barker <willbarker34@gmail.com>
2024-01-04 13:43:53 -0500

    updated endpoint to accept location query param

--- fixit-webapp/app/api/prompts/route.js
+++ fixit-webapp/app/api/prompts/route.js
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request) {
-    const prompt = await request.json()
-    console.log(prompt)
+export async function GET(request) {
+    const searchParams = request.nextUrl.searchParams
+    const prompt = searchParams.get("prompt")
+    const location = searchParams.get("location")
+    
+    console.log("prompt: ", prompt)
+    console.log("location: ", location)
+
+    // Call AWS stuff here
     return NextResponse.json({"yo":"sup"})
 }
