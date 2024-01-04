0005baa0ce006c05a0643099a045588347b07792
Will Barker <willbarker34@gmail.com>
2024-01-04 13:42:35 -0500

    updated search bar elements

--- fixit-webapp/app/page.js
+++ fixit-webapp/app/page.js
@@ -1,12 +1,13 @@
-import { NewPrompt } from "./components/prompt/prompt";
+import { NewPrompt } from "./components/prompt/NewPrompt";
 
 export default function Home() {
 
   const initialPrompt = "Ex. My faucet is leaking";
 
   return (
-    <div className="container mx-auto p-4">
-      <NewPrompt initialPrompt={initialPrompt} />
-    </div>
+      <main className="flex-grow flex flex-col items-center justify-center">
+          <h1 className="mb-4 text-8xl mb-20 font-bold">FixitLink</h1>
+          <NewPrompt />
+      </main>
   );
 }
