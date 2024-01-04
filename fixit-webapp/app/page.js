510bf3626f81ac54d0adcd823125796965b6bb22
Will Barker <willbarker34@gmail.com>
2024-01-04 13:46:49 -0500

    increased margin between header and NewPrompt component

--- fixit-webapp/app/page.js
+++ fixit-webapp/app/page.js
@@ -1,12 +1,9 @@
 import { NewPrompt } from "./components/prompt/NewPrompt";
 
 export default function Home() {
-
-  const initialPrompt = "Ex. My faucet is leaking";
-
   return (
       <main className="flex-grow flex flex-col items-center justify-center">
-          <h1 className="mb-4 text-8xl mb-20 font-bold">FixitLink</h1>
+          <h1 className="mb-20 text-8xl font-bold">FixitLink</h1>
           <NewPrompt />
       </main>
   );
