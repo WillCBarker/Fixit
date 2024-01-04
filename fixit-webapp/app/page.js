86a704892b7e229ad835658de037052fee837f26
Will Barker <willbarker34@gmail.com>
2024-01-04 18:23:32 -0500

    header removed after search results

--- fixit-webapp/app/page.js
+++ fixit-webapp/app/page.js
@@ -1,10 +1,29 @@
+"use client";
 import { NewPrompt } from "./components/prompt/NewPrompt";
+import PromptResults from "./components/prompt/PromptResults"; // Import PromptResults
+import { useState } from "react";
 
 export default function Home() {
+  const [promptResults, setPromptResults] = useState(null);
+
+  const handleResults = (data) => {
+    setPromptResults(data);
+  };
+
   return (
-      <main className="flex-grow flex flex-col items-center justify-center">
-          <h1 className="mb-20 text-8xl font-bold">FixitLink</h1>
-          <NewPrompt />
-      </main>
+    <main className="flex-grow flex flex-col items-center justify-center">
+      {promptResults ? (
+        // Render layout after PromptResults displays data
+        <div className="w-full mt-4 flex flex-col items-center">
+          <PromptResults responseData={promptResults} />
+        </div>
+      ) : (
+        // Render NewPrompt when data is not yet available
+        <>
+            <h1 className="mb-4 text-8xl mb-20 font-bold">FixitLink</h1>
+            <NewPrompt onResults={handleResults} />
+        </>
+      )}
+    </main>
   );
 }
