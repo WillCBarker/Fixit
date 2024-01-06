23079fde10c127b677042768d84e15c16b8dd69b
Will Barker <willbarker34@gmail.com>
2024-01-06 14:28:27 -0500

    move fetching from backend to PromptResults, newprompts now only handles search form

--- fixit-webapp/app/components/prompt/NewPrompt.js
+++ fixit-webapp/app/components/prompt/NewPrompt.js
@@ -1,83 +1,46 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
 import PromptResults from './PromptResults';
+import React, {useState} from "react";
 
-export function NewPrompt({ initialPrompt = ""}) {
-  const [responseData, setResponseData] = useState(null);
-  const promptInputRef = useRef();
-  const locationInputRef = useRef();
+export function NewPrompt({ onSearch}) {
+  // Keeps track of current state of values
+  const [query, setQuery] = useState("");
+  const [location, setLocation] = useState('');
 
-  const submitFormHandler = async (event) => {
-    event.preventDefault();
-    const enteredPrompt = promptInputRef.current.value;
-    const enteredLocation = locationInputRef.current.value;
-
-    try {
-      const response = await fetch(`/api/prompts?prompt=${encodeURIComponent(enteredPrompt)}&location=${encodeURIComponent(enteredLocation)}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      setResponseData(data);
-
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    // Updates using response here
-    console.log("Received data:", responseData);
-    if (responseData) {
-      console.log("Data present", responseData)
-    }
-  }, [responseData]);
-
-  const renderForm = () => {
-    return (
-      <form onSubmit={submitFormHandler} className="rounded bg-white flex flex-col items-center">
-        <div className="relative flex p-2 items-center border rounded-3xl shadow-md">
-          <input
-            type="text"
-            id="prompt"
-            placeholder="Enter problem"
-            className="flex-1 p-2 focus:outline-none sm:w-48 md:w-64 lg:w-96 xl:w-128"
-            ref={promptInputRef}
-          />
-          <span className="border-l h-9 mx-4"></span>
-          <input
-            type="text"
-            id="location"
-            placeholder="Enter location"
-            className="flex-1 p-2 focus:outline-none sm:w-48 md:w-64 lg:w-96 xl:w-128"
-            ref={locationInputRef}
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 ml-4 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 flex items-center rounded-full"
-          >
-            Search
-          </button>
-        </div>
-      </form>
-    );
-  };
+  // Pass input query and location back to home page
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query, location);
+  }
 
   return (
-    <>
-      {renderForm()}
-      {responseData && <PromptResults responseData={responseData} />}
-    </>
+    <form onSubmit={handleSubmit} className="rounded bg-white flex flex-col items-center">
+      <div className="relative flex p-2 items-center border rounded-3xl shadow-md">
+        <input
+          type="text"
+          value={query}
+          placeholder="Enter problem"
+          className="flex-1 p-2 focus:outline-none sm:w-48 md:w-64 lg:w-96 xl:w-128"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <span className="border-l h-9 mx-4"></span>
+        <input
+          type="text"
+          value={location}
+          placeholder="Enter location"
+          className="flex-1 p-2 focus:outline-none sm:w-48 md:w-64 lg:w-96 xl:w-128"
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 ml-4 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 flex items-center rounded-full"
+        >
+          Search
+        </button>
+      </div>
+    </form>
   );
 }
 
 export default NewPrompt;
-
