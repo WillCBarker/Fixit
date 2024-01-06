d54b9a4c505db663c6b71e56e4f885a1b903be25
Will Barker <willbarker34@gmail.com>
2024-01-06 14:32:16 -0500

    added new custom colors

--- fixit-webapp/tailwind.config.js
+++ fixit-webapp/tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      "base-orange": "#f2ce8f",
+      "fixit-green": "#77bc3f"
+    }
   },
   plugins: [],
 }
